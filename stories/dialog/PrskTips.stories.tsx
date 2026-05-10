50c9ee5d420f7f2825ad8e5ed0e936af121207e9
Narumikr <narunarumikumiku396@gmail.com>
2026-05-11 00:05:24 +0900

    fix import storybook obj

--- stories/dialog/PrskTips.stories.tsx
+++ stories/dialog/PrskTips.stories.tsx
@@ -2,7 +2,7 @@ import { PrskTips } from '@/components/dialog/PrskTips'
 
 import { COLORS_SEKAI_KEYS } from '@/styles/sekai-colors'
 
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 
 const meta = {
   title: 'UI/PrskTips',
