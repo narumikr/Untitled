b193d42f91b88ed447d5708bf0ab7ad3ad324856
Narumikr <narunarumikumiku396@gmail.com>
2026-07-12 15:30:32 +0900

    fix lint

--- test/components/tab/Tab.test.tsx
+++ test/components/tab/Tab.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import userEvent from '@testing-library/user-event'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { Tab, TabPanel } from '@/components/tab/Tab'
 
